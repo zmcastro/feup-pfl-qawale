77c5a19edb592fe1e5a2ca9c6403ac6f4bc0aef0
JosA(c) Maria Borges Pires do Couto e Castro <up202006963@edu.fe.up.pt>
2022-10-17 18:56:23 +0100

    committed old work

--- Polynomial.hs
+++ Polynomial.hs
@@ -9,3 +9,11 @@ module Polynomial where
     
     concatPoly :: Polynomial -> Polynomial -> Polynomial
     concatPoly p1 p2 = p1 ++ p2
+
+    -- find function to check existence and find variable inside of polynomial
+    -- returns list index of variable match if it exists, else returns -1 
+    findPoly :: Variable -> Polynomial -> Int
+    findPoly var p = if (null index) then -1 else (head index) 
+        where { index = [i | (i, poly) <- zip [0..] p, var == (fst poly)] }
+    testFind :: Int 
+    testFind = findPoly "x" [("y", [1,4,8]), ("a", [2,4])]
