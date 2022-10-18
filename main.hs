fbdd49dbdf7dae9b9ba3e99fe4e80b7bc9ed0a62
pedrosilva17 <pedroafsuper@gmail.com>
2022-10-19 00:18:34 +0100

    More robust polynomial display and normalization

--- main.hs
+++ main.hs
@@ -1,40 +1,58 @@
 import Data.List
 import Data.Sequence
+import Data.Char
 import Polynomial
 import Data.Binary.Get (skip)
+
+toRemoveLeading :: Char -> Bool
+toRemoveLeading a = (a /= '-') && not (isAlphaNum a)
 
 --refactor p adicionar expoentes ou usar lista com tamanho igual ao maior expoente e guardar por ordem
 
 createPolynomial :: Polynomial
-createPolynomial = [("x", [1,6,5,0]), ("x", [2,0,-7,0]), ("", [8])]
+createPolynomial = [("", [-1,6,-0,-4]), ("w", [-1,-6,-0,-4]), ("y", [-1,-6,-0,-4]), ("x", [-1,-6,-0,-4]), ("z", [-1,-6,-0,-4])]
+
+showPolynomialTrimmed :: Polynomial -> String
+showPolynomialTrimmed (p:ps)
+                            | head a == '-' = '-' : Data.List.drop 2 a
+                            | otherwise = a
+                            where a = dropWhile (toRemoveLeading) (showPolynomial (p:ps))
 
 showPolynomial :: Polynomial -> String
 showPolynomial (p:ps)
                 | Data.List.null (p:ps) = ""
                 | Data.List.null (snd p) = showPolynomial ps
-                | head (snd p) == 0 && Data.List.null ps = ""
-                | head (snd p) == 0 && not (Data.List.null ps) = showPolynomial ((fst p, tail (snd p)):ps)
-                | Data.List.null ps && Data.List.length (snd p) == 1 = printPoly (fst p) (head (snd p)) (Data.List.length (snd p))
-                | otherwise = printPoly (fst p) (head (snd p)) (Data.List.length (snd p)) ++ " + " ++ showPolynomial ((fst p, tail (snd p)):ps)
+                | Data.List.null ps && Data.List.length (snd p) == 1 && head (snd p) < 0 = " - " ++ printPolynomial (fst p) (abs (head (snd p))) (Data.List.length (snd p))
+                | Data.List.null ps && Data.List.length (snd p) == 1 && head (snd p) > 0 = " + " ++ printPolynomial (fst p) (head (snd p)) (Data.List.length (snd p))
+                | head (snd p) == 0 = printPolynomial (fst p) (head (snd p)) (Data.List.length (snd p)) ++ showPolynomial ((fst p, tail (snd p)):ps)
+                | head (snd p) < 0 = " - " ++ printPolynomial (fst p) (abs (head (snd p))) (Data.List.length (snd p)) ++ showPolynomial ((fst p, tail (snd p)):ps)
+                | otherwise = " + " ++ printPolynomial (fst p) (head (snd p)) (Data.List.length (snd p)) ++ showPolynomial ((fst p, tail (snd p)):ps)
 
 sortAndNormalize :: Polynomial -> Polynomial
-sortAndNormalize (p:ps) = normalizePolynomial (Data.List.sortOn fst (p:ps))
+sortAndNormalize (p:ps)
+                        | (fst (head a)) == "" = tail (a) ++ [head a]
+                        | otherwise = a
+                        where a = normalizePolynomial(Data.List.sortOn fst (p:ps))
 
 normalizePolynomial :: Polynomial -> Polynomial
 normalizePolynomial (p:ps)
-                            | Data.List.length ps == 1 && fst p /= fst (head ps) = (p:ps)
+                            | Data.List.null ps && not (Data.List.null (fst p)) = [p]
+                            | Data.List.length ps == 1 && fst p /= fst (head ps) = p:ps
                             | Data.List.length ps == 1 && fst p == fst (head ps) = [(fst p, Data.List.zipWith (+) (snd p) (snd (head ps)))]
-                            | fst p == fst (head ps) = (fst p, Data.List.zipWith (+) (snd p) (snd (head ps))) : normalizePolynomial (tail ps)
-                            | otherwise = normalizePolynomial (tail ps)
+                            | Data.List.null (fst p) && Data.List.length (snd p) /= 1 && Data.List.null ps = [(fst p, [Data.List.sum (snd p)])]
+                            | Data.List.null (fst p) && Data.List.length (snd p) /= 1 = normalizePolynomial ((fst p, [Data.List.sum (snd p)]) : ps)
+                            | fst p == fst (head ps) = normalizePolynomial ((fst p, Data.List.zipWith (+) (snd p) (snd (head ps))) : tail ps)
+                            | otherwise = p : normalizePolynomial ps
 
-printPoly :: String -> Int -> Int -> String
-printPoly var coef exp
-                    | exp == 0 = show coef
+printPolynomial :: String -> Int -> Int -> String
+printPolynomial var coef exp
+                    | coef == 0 = ""
+                    | coef == 1 && Data.List.null var = show coef
                     | coef == 1 && exp == 1 = var
                     | coef == 1 && exp /= 1 = var ++ "^" ++ show exp
                     | coef /= 1 && exp == 1 = show coef ++ var
                     | coef /= 1 && exp /= 1 = show coef ++ var ++ "^" ++ show exp
-                    | otherwise = error "Error on printPoly"
+                    | otherwise = error "Error on printPolynomial"
 
 addPolynomial :: Polynomial -> Polynomial -> Polynomial
 addPolynomial [] [] = []
